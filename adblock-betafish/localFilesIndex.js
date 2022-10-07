4a6d4d9e5ae51eaa8159e1035d5d614a44d611ed
BrentM <brent@getadblock.com>
2022-10-07 10:39:18 +0200

    Release version 5.1.3

--- adblock-betafish/localFilesIndex.js
+++ adblock-betafish/localFilesIndex.js
@@ -1,25 +1,101 @@
 function getAvailableFiles() {
 return {
 jquery: {
-"jquery-1.10.2.min.js": 93107,
-"jquery-1.11.0.min.js": 96381,
-"jquery-1.11.1.min.js": 95786,
-"jquery-1.11.2.min.js": 95931,
-"jquery-1.11.3.min.js": 95957,
-"jquery-1.12.4.min.js": 97163,
-"jquery-1.3.2.min.js": 57254,
-"jquery-1.4.2.min.js": 72174,
-"jquery-1.7.1.min.js": 93868,
-"jquery-1.7.2.min.js": 94840,
-"jquery-1.8.2.min.js": 93435,
-"jquery-1.8.3.min.js": 93636,
-"jquery-1.9.1.min.js": 92629,
-"jquery-2.1.1.min.js": 84245,
-"jquery-2.1.3.min.js": 84320,
-"jquery-2.1.4.min.js": 84345,
-"jquery-2.2.4.min.js": 85578,
-"jquery-3.1.1.min.js": 86709,
-"jquery-3.2.1.min.js": 86659,
+"jquery-1.10.2.min.js":   File: "jquery-1.10.2.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076425  Available: 213076425
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.11.0.min.js":   File: "jquery-1.11.0.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076425  Available: 213076425
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.11.1.min.js":   File: "jquery-1.11.1.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076425  Available: 213076425
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.11.2.min.js":   File: "jquery-1.11.2.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.11.3.min.js":   File: "jquery-1.11.3.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.12.4.min.js":   File: "jquery-1.12.4.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.3.2.min.js":   File: "jquery-1.3.2.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.4.2.min.js":   File: "jquery-1.4.2.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.7.1.min.js":   File: "jquery-1.7.1.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.7.2.min.js":   File: "jquery-1.7.2.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.8.2.min.js":   File: "jquery-1.8.2.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.8.3.min.js":   File: "jquery-1.8.3.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-1.9.1.min.js":   File: "jquery-1.9.1.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-2.1.1.min.js":   File: "jquery-2.1.1.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-2.1.3.min.js":   File: "jquery-2.1.3.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-2.1.4.min.js":   File: "jquery-2.1.4.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076424  Available: 213076424
+Inodes: Total: 999        Free: 1000000,
+"jquery-2.2.4.min.js":   File: "jquery-2.2.4.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076423  Available: 213076423
+Inodes: Total: 999        Free: 1000000,
+"jquery-3.1.1.min.js":   File: "jquery-3.1.1.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076423  Available: 213076423
+Inodes: Total: 999        Free: 1000000,
+"jquery-3.2.1.min.js":   File: "jquery-3.2.1.min.js"
+    ID: 100000000 Namelen: 255     Type: v9fs
+Block size: 4096       Fundamental block size: 4096
+Blocks: Total: 244891903  Free: 213076423  Available: 213076423
+Inodes: Total: 999        Free: 1000000,
 },
 };
 }
