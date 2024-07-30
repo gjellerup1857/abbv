5d379bddde16a44960b6b4ba21b29b4bbd27a317
BrentM <brent@getadblock.com>
2024-07-30 15:55:13 -0600

    remove duplicate type

--- host/adblock/adblock-betafish/onpage-dialog/background/middleware/ipm-onpage-dialog.types.ts
+++ host/adblock/adblock-betafish/onpage-dialog/background/middleware/ipm-onpage-dialog.types.ts
@@ -15,7 +15,7 @@
  * along with AdBlock.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { LicenseStateBehavior, Command } from '../../../ipm/background';
+import { Command } from '../../../ipm/background';
 
 /**
  * Timing name
@@ -24,28 +24,6 @@ export enum Timing {
     afterWebAllowlisting = 'after_web_allowlisting',
     revisitWebAllowlisted = 'revisit_web_allowlisted_site',
     afterNavigation = 'after_navigation',
-}
-
-/**
- * On-page dialog behavior
- */
-export interface DialogBehavior extends LicenseStateBehavior {
-    /**
-     * On-page dialog display duration in minutes
-     */
-    displayDuration: number;
-    /**
-     * Target page to open when interacting with the on-page dialog
-     */
-    target: string;
-    /**
-     * When to open on-page dialog
-     */
-    timing: Timing;
-    /**
-     * Domain(s) which the OPD should or should not be shown
-     */
-    domain_list?: string;
 }
 
 /**
