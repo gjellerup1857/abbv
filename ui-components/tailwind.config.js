e93fd8a68f0c6c00f21129bfee6a147e9825728a
Sarah Groff Hennigh-Palermo <109884-sarahghp@users.noreply.gitlab.com>
2024-08-22 09:41:43 +0000

    feat: Add Tailwind and Basic Theming for Adblock [EXT-76]
    The purpose of this MR is to add scaffolding for themed Tailwind for our new UI framework components. This consists of:
    
    - Reviewing the current status of theme variables in Adblock to pull out the themed colors (this was complicated! :arrow_right: see !36)
    - Adding the library and related plumbing
    - Adding the theme colors to our CSS as variables and then using those variables to set Tailwind values (see `ui-components/tailwind.config.js` and `ui-components/src/adblock-color-themes.css`) for how that integration works
    - Integrating this functionality into the host, by adding the `data-*` attributes to the Adblock host
    - Creating scaffolding for hosting themes and eventually components in a little website

---
+++ ui-components/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./website/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // We can take these out of extend and put in theme once we have all the colors we want
+      // if we want to prohibit other Tailwind-defined colors
+      colors: {
+        "theme-primary": "var(--theme-primary)",
+        "theme-secondary": "var(--theme-secondary)",
+        "theme-text-primary": "var(--theme-text-primary)",
+        "theme-text-secondary": "var(--theme-text-secondary)",
+        "theme-link-color": "var(--theme-link-color)",
+        "theme-accent-dark": "var(--theme-accent-dark)",
+        "theme-accent-light": "var(--theme-accent-light)",
+        "theme-text-accent": "var(--theme-text-accent)",
+      },
+    },
+  },
+  plugins: [],
+}
