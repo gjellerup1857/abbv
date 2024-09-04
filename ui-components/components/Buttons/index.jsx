2fbeb7af8eebc8c384d00182a1c468502d6241a9
Sarah GHP <sarah.groff.palermo@gmail.com>
2024-09-04 17:33:59 +0200

    Adds hover styles

--- ui-components/components/Buttons/index.jsx
+++ ui-components/components/Buttons/index.jsx
@@ -51,12 +51,13 @@ export const Button = ({
 
   const disabledStyles = disabled ? ['opacity-50', 'pointer-events-none'] : [];
   const disabledWrapperStyles = disabled ? ['cursor-not-allowed'] : [];
+
   const kindStyles = {
-    filled: ['bg-theme-button-primary', 'text-theme-button-secondary'],
-    outline: ['text-theme-button-primary', 'border', 'border-theme-button-primary'],
-    text: ['text-theme-accent-dark'],
-    link: ['text-theme-link-color', 'inline'],
-    punched: ['bg-theme-accent-dark', 'text-theme-secondary'],
+    filled: ['bg-theme-button-primary', 'text-theme-button-secondary', 'hover:drop-shadow-md', 'hover:bg-theme-link-color'],
+    outline: ['text-theme-button-primary', 'border', 'border-theme-button-primary', 'hover:bg-theme-button-primary', 'hover:text-theme-button-secondary'],
+    text: ['text-theme-accent-dark', 'hover:text-theme-link-color'],
+    link: ['text-theme-link-color', 'inline', 'hover:text-theme-text-accent'],
+    punched: ['bg-theme-accent-dark', 'text-theme-secondary', 'hover:bg-theme-link-color'],
   };
 
   const customStyles = colorOverrides ?? kindStyles[kind];
