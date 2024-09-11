8e5975f260dd05862420f1ea1341ad21eea928c9
Sarah Groff Hennigh-Palermo <109884-sarahghp@users.noreply.gitlab.com>
2024-09-11 16:34:28 +0000

    feat: Add Button Component to UI Components, Part 2 [EXT-242]
    This MR takes care of the tasks known to be outstanding in !58:
    
    * adding hover/active states
    * adding disabled state
    * refining themes: values for some elements were incorrect and resulted in illegible text
    * adding type annotations
    * completing tests
    
    Plus tasks added by integrating storybook:
    
    * adding disabled state to Storybook
    * ~~removing `website` directory~~ reverted see MR coming soon
    
    It does not address:
    
    * Not all the values are high contrast. This reflects the values currently existing in the system. [EXT-260](https://eyeo.atlassian.net/browse/EXT-260) has been opened to revisit these values, but this is outside the scope of this code.
    
    * The README is out of sync with how to document using Storybook. I have added [EXT-287](https://eyeo.atlassian.net/browse/EXT-287) for the next sprint.
    
    See eyeo/extensions/extensions!78

--- ui-components/website/component-examples/Buttons.jsx
+++ ui-components/website/component-examples/Buttons.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import { Button } from '@components'
+import { Button, buttonKinds } from '@components'
 import { themeNames } from './Themes';
-
-const buttonKinds = ['filled', 'outline', 'text', 'link', 'punched'];
 
 export const Buttons = () => {
   const [showSorryButton, setShowSorryButton] = useState(false);
@@ -33,13 +31,21 @@ export const Buttons = () => {
             Object.keys(themeNames).map((name) => (
               <div data-extension="adblock" data-theme={ name } key={ name } className="flex flex-col items-start w-full">
                 <p> { name } </p>
-                <div className="flex w-full gap-x-2 bg-theme-secondary p-4">
+                <div className="flex flex-wrap w-full gap-x-2 bg-theme-secondary p-4">
+                  {
+                    buttonKinds.map((kind) => (
+                      <Button text="do not click me" key={ kind } kind={ kind } ariaLabel="ow-ow-ow" onClick={ clickFn } />
+                    ))
+                  }
+                </div>
+                <div className="flex flex-wrap w-full gap-x-2 bg-theme-secondary p-4">
                 {
                   buttonKinds.map((kind) => (
-                    <Button text="do not click me" key={ kind } kind={ kind } ariaLabel="ow-ow-ow" onClick={ clickFn } />
+                    <Button text="I am unclickable" disabled key={ `disabled-${kind}` } kind={ kind } ariaLabel="ow-ow-ow" onClick={ clickFn } />
                   ))
                 }
                 </div>
+
               </div>
             ))
           }
