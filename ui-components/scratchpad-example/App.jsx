b2b9cfad393202cea1c116f8edf048e97a974c39
Sarah GHP <sarah.groff.palermo@gmail.com>
2024-09-12 12:11:33 +0200

    Remove tracking of scratchpad, add example

---
+++ ui-components/scratchpad-example/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex">
+      <p>I am a place to try things out! But be sure you document the results in <code className="text-rose-600">docs/</code> when you are done.</p>
+    </div>
+  )
+}
+
+export default App;
