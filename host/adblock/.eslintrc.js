ccab970075c633bd2c1e8b9531e443e3664c5b56
Thomas Greiner <thomas@adblockplus.org>
2024-10-15 11:34:36 +0200

    Removed host/adblock/eslint/legacy.js

--- host/adblock/.eslintrc.js
+++ host/adblock/.eslintrc.js
@@ -1,19 +1,7 @@
 /* eslint-env node */
 
-/**
- * NOTE: This file just points to the actual configuration file found in the
- * `eslint` directory. Do not make changes to this file directly, but instead
- * to the configuration file this file points to.
- */
+"use strict";
 
 module.exports = {
-  extends: "./eslint/legacy.js",
-  overrides: [
-    {
-      files: ["*.jsx"],
-      rules: {
-        strict: "off", // Disable 'strict' rule for JSX files
-      },
-    },
-  ],
+  extends: "./eslint/love.js"
 };
