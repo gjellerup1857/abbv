1d638cc0c51e5952d6d18bd4a635bb53c66d45db
Sarah GHP <sarah.groff.palermo@gmail.com>
2024-10-29 18:11:32 +0100

    Add conditional UI check

--- host/adblock/adblock-betafish/options/react-components/main.jsx
+++ host/adblock/adblock-betafish/options/react-components/main.jsx
@@ -2,6 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app.jsx";
 
-const container = document.getElementById("react-options");
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
+const checkAndAddApp = () => {
+  const url = new URL(window.top.location.href);
+  const showNewUI = url.searchParams.has("newUi");
+
+  if (!showNewUI) {
+    return;
+  }
+
+  const container = document.getElementById("react-options");
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
+
+  const oldUI = document.getElementById("jq-options");
+  oldUI.style.display = "none";
+}
+
+checkAndAddApp();
