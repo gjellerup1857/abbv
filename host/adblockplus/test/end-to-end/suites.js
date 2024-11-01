82282f2bf82eefd24d855258ab2efe7a396c34db
Anton Smirnov <a.smirnov@eyeo.com>
2024-11-01 18:36:19 +0500

    Add test draft

--- host/adblockplus/test/end-to-end/suites.js
+++ host/adblockplus/test/end-to-end/suites.js
@@ -18,38 +18,40 @@
 "use strict";
 
 exports.suites = {
-  all: ["./tests/**.js"],
+  all: ["./tests/test-eye**.js"],
   filterlists: [
-    "./tests/test-smoke-*.js",
-    "./tests/test-advanced-tab-filter-lists.js",
-    "./tests/test-built-in-filter-list-dropdown.js",
-    "./tests/test-options-page-acceptable-ads.js",
-    "./tests/test-popup-main.js",
-    "./tests/test-abp-premium-ui-free-user.js",
-    "./tests/public-api/index.js",
-    "./tests/test-unlock-premium.js"
+    "./tests/test-eye**.js"
+    // "./tests/test-smoke-*.js",
+    // "./tests/test-advanced-tab-filter-lists.js",
+    // "./tests/test-built-in-filter-list-dropdown.js",
+    // "./tests/test-options-page-acceptable-ads.js",
+    // "./tests/test-popup-main.js",
+    // "./tests/test-abp-premium-ui-free-user.js",
+    // "./tests/public-api/index.js",
+    // "./tests/test-unlock-premium.js"
   ],
   oldbrowsers: [
-    "./tests/test-abp-premium-cookiefl-premium-users.js",
-    "./tests/test-abp-premium-dcfl-premium-users.js",
-    "./tests/test-abp-premium-downgrade.js",
-    "./tests/test-abp-premium-get-started.js",
-    "./tests/test-abp-premium-links-for-free-users.js",
-    "./tests/test-abp-premium-links-for-premium-users.js",
-    "./tests/test-abp-premium-onboarding-free-user.js",
-    "./tests/test-abp-premium-onboarding-premium-user.js",
-    "./tests/test-abp-premium-one-click-allow.js",
-    "./tests/test-abp-premium-ui-free-user.js",
-    "./tests/test-abp-premium-ui-premium-user.js",
-    "./tests/test-advanced-tab-customisations.js",
-    "./tests/test-advanced-tab-filter-lists.js",
-    "./tests/test-allowlisted-websites-tab.js",
-    "./tests/test-built-in-filter-list-dropdown.js",
-    "./tests/test-integration-subscriptions.js",
-    "./tests/test-one-click-allow.js",
-    "./tests/test-options-page-language.js",
-    "./tests/test-options-page-recommended-filters.js",
-    "./tests/test-smoke-*.js"
+    "./tests/test-eye**.js"
+    // "./tests/test-abp-premium-cookiefl-premium-users.js",
+    // "./tests/test-abp-premium-dcfl-premium-users.js",
+    // "./tests/test-abp-premium-downgrade.js",
+    // "./tests/test-abp-premium-get-started.js",
+    // "./tests/test-abp-premium-links-for-free-users.js",
+    // "./tests/test-abp-premium-links-for-premium-users.js",
+    // "./tests/test-abp-premium-onboarding-free-user.js",
+    // "./tests/test-abp-premium-onboarding-premium-user.js",
+    // "./tests/test-abp-premium-one-click-allow.js",
+    // "./tests/test-abp-premium-ui-free-user.js",
+    // "./tests/test-abp-premium-ui-premium-user.js",
+    // "./tests/test-advanced-tab-customisations.js",
+    // "./tests/test-advanced-tab-filter-lists.js",
+    // "./tests/test-allowlisted-websites-tab.js",
+    // "./tests/test-built-in-filter-list-dropdown.js",
+    // "./tests/test-integration-subscriptions.js",
+    // "./tests/test-one-click-allow.js",
+    // "./tests/test-options-page-language.js",
+    // "./tests/test-options-page-recommended-filters.js",
+    // "./tests/test-smoke-*.js"
   ],
-  smoke: ["./tests/test-smoke-*.js"]
+  smoke: ["./tests/test-eye**.js"]
 };
---
+++ host/adblockplus/test/end-to-end/tests/test-eyeometry.js
@@ -0,0 +1,64 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+
+const {expect} = require("chai");
+const {isFirefox} = require("../helpers");
+
+it("sends telemetry request and saves the data in the storage", async function()
+{
+  const timeout = 10000;
+  const timeoutMsg = `No storage data after ${timeout}ms`;
+  let data;
+
+  try
+  {
+    await browser.waitUntil(async() =>
+    {
+      const key = "ewe:telemetry";
+      data = await browser.storage.local.get([key]);
+      if (data)
+        data = data[key];
+      if (data)
+        return true;
+    },
+                            {
+                              timeout,
+                              interval: 100,
+                              timeoutMsg
+                            });
+  }
+  catch (e)
+  {
+    if (!isFirefox())
+    {
+      throw e;
+    }
+    else
+    {
+      // It's Firefox and no storage data was saved, all good
+      return;
+    }
+  }
+
+  expect(data).toEqual(expect.objectContaining({
+    firstPing: expect.any(String),
+    lastPing: expect.any(String),
+    lastPingTag: expect.any(String)
+  }));
+});
