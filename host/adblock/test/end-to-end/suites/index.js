d55496d60bf8b5f7747a94986092b2704713b91e
Aga <a.czyzewska@eyeo.com>
2024-11-04 16:10:41 +0100

    Remove only from describe

--- host/adblock/test/end-to-end/suites/index.js
+++ host/adblock/test/end-to-end/suites/index.js
@@ -60,7 +60,7 @@ export default () => {
   });
 
   describe("Premium Tests", getPremium);
-  describe.only("Eyeometry", eyeometry);
+  describe("Eyeometry", eyeometry);
 
   // Needs to be the last suite to run because the extension gets uninstalled
   describe("Smoke Tests - Uninstall", uninstall);
