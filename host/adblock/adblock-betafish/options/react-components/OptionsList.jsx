46ce5179f6dc11fbaffbbfacd96db7bb061dcbd9
Sarah Groff Hennigh-Palermo <109884-sarahghp@users.noreply.gitlab.com>
2024-11-19 15:35:00 +0000

    feat: Adblock Options in React, Part 1 [EXT-420]
    ## Description
    
    This is the first step in moving the Adblock Options page to React, using the components from the component library. It:
    * adds the ability to use React in the Adblock options page
    * adds the list of options; 
    * applies CSS across the themes; 
    * rectifies clashes between the components CSS and existing host CSS, including reverting some changes to the composition of variables (see #1 for more details)
    
    This work is gated behind a param. One can use `newUi=1` to see the React version. 
    
    | old | new |
    |-----|-----|
    |  ![Screenshot_2024-11-12_at_18.32.42](/uploads/a47ea1e58dbcd60ce8a17fe8afdd996c/Screenshot_2024-11-12_at_18.32.42.png)  |  ![Screenshot_2024-11-12_at_19.04.16](/uploads/78915a39c21456299ea4a06725801073/Screenshot_2024-11-12_at_19.04.16.png)  |
    
    **They do not look identical.** In fact, there are a number of un-resolved elements left to resolve.
    
    ### Outstanding Items: Functionality (can be done in further MRs, must be finished before we ship)
    
    * Reading preferences and populating boxes from the state (in progress)
    * Conditionally hiding / showing boxes with informational text (with above)
    * Adding click behavior
    * Tests (we should set up unit tests using the same Vitest setup we have in UI components and test the logic and events)
    
    ### Issues Left to Resolve (can be done in further MRs, must be finished before we ship)
    
    * The style of toggle used in the current version was not implemented for the component library. At the same time, the toggle we have, does not appear correctly in the dark theme and bulks up the line height with its set size. I believe the better resolution for this is to implement the second toggle style, but it does need to be resolved.
    * The checkbox style for unselected items differs from the current status and the component library needs to be updated, including some changes to theme accent colors (which I expected)
    
    ### Issues to Be Aware of for the Future
    
    * We need fix the slightly different APIs between checkbox and toggle; right now with a dynamic component, we are passing both options when we should pass one, same-named option
    
    * Adblock needs to address how it loads different tabs. Right now, to work least invasively with the architecture, the script for React is being loaded inline in the general page. This is limited and ok for now, but not a real solution.
    
    ## Checklist:
    
    <!-- Go over all the following points, keep those that apply and mark them with an `x` whenever the task is completed -->
    
    - ~~[ ] I have updated the release notes~~ It is unreleased
    - [x] My changes didn't require a documentation update
    - [x] My changes didn't require new tests — As noted above, tests will need to be added before this is launched, but it is not releasing anything
    - [ ] All new and existing tests passed
    
    See eyeo/extensions/extensions!209

---
+++ host/adblock/adblock-betafish/options/react-components/OptionsList.jsx
@@ -0,0 +1,78 @@
+import { Checkbox, Icon, Link, ToggleSwitch } from "@eyeo/ext-ui-components";
+import { translate } from "./utils";
+
+const changeHandler = () => console.log("change is possible");
+
+const OptionItem = ({
+  name,
+  textKey,
+  extraInfo,
+  helpLink,
+  additionalInfoLink,
+  subOptions,
+  isSubOption = false,
+  selector: Selector = Checkbox,
+}) => {
+  const subOptionLiClasses = isSubOption ? "pl-6" : "first-of-type:border-none";
+  const itemClasses = ["text-lg  border-t border-theme-accent-light", subOptionLiClasses].join(" ");
+
+  const alignementClasses = isSubOption ? "items-baseline" : "items-center";
+  const outerDivClasses = ["flex justify-between py-2", alignementClasses].join(" ");
+  const innerDivClasses = ["flex", alignementClasses].join(" ");
+
+  return (
+    <li key={name} className={itemClasses}>
+      <div className={outerDivClasses}>
+        <div className={innerDivClasses}>
+          {/* The Toggle and Checkbox APIs need to be aligned */}
+          <Selector id={name} name={name} onChange={changeHandler} />
+          <label className="ml-4" for={name}>
+            {translate(textKey)}
+          </label>
+        </div>
+        <div className="flex items-center">
+          {additionalInfoLink && (
+            <Link href={additionalInfoLink.href}>{translate(additionalInfoLink.textKey)}</Link>
+          )}
+          {helpLink && (
+            <span className="inline-block ml-2">
+              <Link href={helpLink}>
+                <Icon
+                  name="live-help"
+                  ariaLabel="learn_more_without_period"
+                  className="fill-theme-accent-dark hover:fill-theme-link-color"
+                  size="sm"
+                />
+              </Link>
+            </span>
+          )}
+        </div>
+      </div>
+      <div className="mb-2 -mt-2.5">
+        {extraInfo && <span className="text-base italic pl-8">{translate(extraInfo)}</span>}
+      </div>
+      <div>
+        {subOptions && (
+          <ul>
+            {subOptions.map((option) => (
+              <OptionItem isSubOption selector={ToggleSwitch} {...option} />
+            ))}
+          </ul>
+        )}
+      </div>
+    </li>
+  );
+};
+
+export const OptionsList = ({ items }) => {
+  return (
+    <>
+      <h1>{translate("generaloptions2")}</h1>
+      <ul>
+        {items.map((item) => (
+          <OptionItem {...item} />
+        ))}
+      </ul>
+    </>
+  );
+};
