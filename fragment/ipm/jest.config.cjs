c402f721e621a27a5fcfc613acf36334375405ce
Stefano Pacifici <stefano.pacifici@gmail.com>
2024-11-22 22:38:08 +0100

    babel problems fixed

---
+++ fragment/ipm/jest.config.cjs
@@ -0,0 +1,23 @@
+/* eslint-env node */
+
+"use strict";
+
+/**
+ * For a detailed explanation regarding each configuration property and type
+ * check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+module.exports = {
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageReporters: ["json", "html", "text"],
+    moduleNameMapper: {
+        "^~/(.*)$": "<rootDir>/src/$1"
+    },
+    resetMocks: true,
+    restoreMocks: true,
+    // setupFiles: ["jest-webextension-mock", "./mocks/js/jest-polyfill.js"],
+    setupFiles: ["jest-webextension-mock"],
+    // testEnvironment: "./mocks/js/jest-jsdom.mjs",
+    testMatch: ["**/*.spec.ts"]
+};
