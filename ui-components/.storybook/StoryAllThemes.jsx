e73d204ba214de1ce2b3ea29e96f9b04ff3b527d
Sarah Groff Hennigh-Palermo <109884-sarahghp@users.noreply.gitlab.com>
2024-11-26 11:14:24 +0000

    fix: Match checkbox component design to current [noissue]
    ## Description
    
    Component library additions and cleanup. 
    
    * Matches current Adblock checkbox styling 
    * Adds Storybook utility to toggle between the two possible backgrounds (options & popup / primary & secondary).
    
    | adblock current | adblock react before | adblock react after | abp react before | abp react after |
    |-----------------|----------------------|---------------------|------------------|-----------------|
    | ![Screenshot_2024-11-22_at_15.24.44](/uploads/0e3386dd33436a919e7897e355cce8e0/Screenshot_2024-11-22_at_15.24.44.png)| ![Screenshot_2024-11-12_at_19.04.16](/uploads/c37ebdd181f978bc7ad65f77a171f240/Screenshot_2024-11-12_at_19.04.16.png) | ![Screenshot_2024-11-22_at_15.24.57](/uploads/a3a6f0405ae8ef99f873a9c421d83874/Screenshot_2024-11-22_at_15.24.57.png) | ![Screenshot_2024-11-22_at_15.41.45](/uploads/5e86154a2e38166aeef5576b598cc75a/Screenshot_2024-11-22_at_15.41.45.png)                | ![Screenshot_2024-11-22_at_15.33.05](/uploads/5997089a26686e6988be83457693a05f/Screenshot_2024-11-22_at_15.33.05.png)                | 
    
    ## Motivation and Context
    
    As mentioned in !209, I have concerns about React changes being appropriate for Adblock with a new checkbox design style. I expect these will change going forward, but this is a quick update to align a bit more with what exists now. Also, because these are designed to only show on the primary background (and others only on secondary), I added the ability to toggle between them, since there is currently no correct answer.
    
    ## Checklist:
    - [x] My changes didn't require a documentation update
    - [x] My changes didn't require new tests
    - [x] All new and existing tests passed
    
    See eyeo/extensions/extensions!281

--- ui-components/.storybook/StoryAllThemes.jsx
+++ ui-components/.storybook/StoryAllThemes.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Story } from '@storybook/blocks';
 import PropTypes from 'prop-types';
 
@@ -8,14 +9,34 @@ import PropTypes from 'prop-types';
  * @constructor
  */
 export default function StoryAllThemes({ themes }) {
+  const [backgroundColor, updateBackgroundColor] = useState('bg-theme-primary');
+  const onThemeRadioChange = (evt) => {updateBackgroundColor(evt.target.value)}
+
+  const baseClasses = 'flex items-center justify-center gap-2 px-10 py-5 rounded';
+  const displayClasses = [baseClasses, backgroundColor].join(' ');
+
   return (
     <div>
-      <h3 id="preview-all-themes">
-        All Themes
-      </h3>
+      <div className="flex items-baseline justify-between">
+        <h3 id="preview-all-themes">
+          All Themes
+        </h3>
+        <div className="flex items-center justify-end gap-x-2">
+          <p>Background color:</p>
+          <div  className="flex items-center">
+            <input type="radio" id="primary" name="bkg-color" value="bg-theme-primary" checked={backgroundColor === 'bg-theme-primary'} onChange={onThemeRadioChange}/>
+            <label className="text-sm ml-1" htmlFor="primary">Primary</label>
+          </div>
+          <div  className="flex items-center">
+            <input type="radio" id="secondary" name="bkg-color" value="bg-theme-secondary" checked={backgroundColor === 'bg-theme-secondary'} onChange={onThemeRadioChange}/>
+            <label className="text-sm ml-1" htmlFor="secondary">Secondary</label>
+          </div>
+        </div>
+      </div>
       <p>
-        Showcases the first story in all available themes for each extension.
+        Showcases the first story in all available themes for each extension. Note that some items are designed for one bckground color or the other, depending on if they are popup or options only, or designed for both.
       </p>
+
       {
         themes.map(({extension, name}) => (
           <div key={`${extension}-${name}`}>
@@ -25,7 +46,7 @@ export default function StoryAllThemes({ themes }) {
             <div
               data-extension={extension.toLowerCase()}
               data-theme={name.toLowerCase()}
-              className="flex items-center justify-center gap-2 px-10 py-5 bg-theme-secondary rounded"
+              className={displayClasses}
             >
               <Story/>
             </div>
