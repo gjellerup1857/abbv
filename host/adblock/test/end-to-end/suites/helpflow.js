1c7f5c3b012dc01b15be325a92acebc99e7dfc8f
Toni Feliu <tonimellodic@gmail.com>
2024-11-27 13:23:52 +0100

    Ensure the right helpflow UI has effectively loaded

--- host/adblock/test/end-to-end/suites/helpflow.js
+++ host/adblock/test/end-to-end/suites/helpflow.js
@@ -8,31 +8,26 @@ import {
   waitAndClickOnElement,
 } from "../utils/driver.js";
 
-// Utility function to initialize and open the Helpflow
-async function initializeHelpflow() {
-  const tabId = await getTabId(getOptionsHandle());
-  await initPopupPage(tabId);
-  await driver.navigate().refresh();
-  const popupWindow = await driver.getWindowHandle();
-  await driver.switchTo().window(popupWindow);
-  return tabId;
-}
+const seeAdSelector = "[i18n='see_ad']";
 
 async function clickHelpIcon() {
   await openNewTab(blockHideUrl);
-  const tabId = await initializeHelpflow();
+  const tabId = await getTabId(getOptionsHandle());
+
+  await initPopupPage(tabId);
   await waitAndClickOnElement("#help_link", 3000);
   const currentURL = await driver.getCurrentUrl();
   const newUrl = `${currentURL}&tabId=${tabId}`;
-  await driver.get(newUrl);
-  expect(newUrl).toContain(`${tabId}`);
+  await driver.navigate().to(newUrl);
+  // Ensure the right helpflow UI has effectively loaded
+  await getDisplayedElement(seeAdSelector);
 }
 
 export default () => {
   it("initiates helpflow for first time ad", async function () {
     await clickHelpIcon();
 
-    await waitAndClickOnElement("[i18n='see_ad']", 3000);
+    await waitAndClickOnElement(seeAdSelector, 3000);
     await waitAndClickOnElement("[i18n='first_time_seeing_ad']", 3000);
     await waitAndClickOnElement(".button.help-button", 1000);
     await waitAndClickOnElement("[i18n='updating_filter_lists']");
@@ -43,15 +38,15 @@ export default () => {
     await addFiltersToAdBlock("@@localhost:3005$document");
     await clickHelpIcon();
 
-    await waitAndClickOnElement("[i18n='see_ad']", 3000);
+    await waitAndClickOnElement(seeAdSelector, 3000);
     await waitAndClickOnElement("[i18n='first_time_seeing_ad']", 3000);
 
     await waitAndClickOnElement(".button.help-button", 1000);
-    await waitAndClickOnElement("[i18n='page_is_whitelisted']");
     const allowlistText = await getDisplayedElement("[i18n='page_is_whitelisted']");
     expect(await allowlistText.getText()).toContain(
       "This page is on your allowlist, which is why you're seeing an ad.",
     );
+    await allowlistText.click();
     await waitAndClickOnElement("[i18n='no']");
     await waitAndClickOnElement("[i18n-aria-label='back_help_flow']");
     await waitAndClickOnElement("[i18n='yes']");
@@ -61,7 +56,7 @@ export default () => {
     await addFiltersToAdBlock("@@localhost:3005$document");
     await clickHelpIcon();
 
-    await waitAndClickOnElement("[i18n='see_ad']", 3000);
+    await waitAndClickOnElement(seeAdSelector, 3000);
     await waitAndClickOnElement("[i18n='first_time_seeing_ad']", 3000);
     await waitAndClickOnElement(".button.help-button", 3000);
     await getDisplayedElement("[i18n='page_is_whitelisted']");
@@ -72,7 +67,7 @@ export default () => {
   it("initiates helpflow for ads everywhere", async function () {
     await clickHelpIcon();
 
-    await waitAndClickOnElement("[i18n='see_ad']", 5000);
+    await waitAndClickOnElement(seeAdSelector, 5000);
     await waitAndClickOnElement("[i18n='see_ad_everywhere']", 3000);
 
     await waitAndClickOnElement("[i18n='learn_more_to_resolve']", 3000);
